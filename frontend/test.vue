1ae19bdc415a406e99da83ca4428dcc16f8004bf
brichbash <radmir.baembitov@jetbrains.com>
2022-11-14 05:49:21 +0100

    init commit

---
+++ frontend/test.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    computed: {
+      licenseKey() {
+        return 'somethigng'
+      },
+      secondProp: function() {
+        return 12
+      },
+      isLicenseActive() {
+        this.licenseKey !== ''
+        this.licenseKey !== 12
+
+        this.secondProp !== ''
+        this.secondProp !== 12
+      }
+    }
+  }
+</script>
